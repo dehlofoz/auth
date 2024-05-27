30c3e8dbbf596454931d89ea7ab1618da77f5f81
dehlofoz <150254552+dehlofoz@users.noreply.github.com>
2024-05-27 21:04:32 +0300

    Update apiRouter.js

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -6,9 +6,8 @@ const authRouter = require("./auth")
 
 
 
-
+apiRouter.use("/api", gamesRouter);
+apiRouter.use("/api", categoriesRouter);
 apiRouter.use("/api", usersRouter);
-apiRouter.use("/api", gamesRouter)
-apiRouter.use("/api", categoriesRouter)
 apiRouter.use("/api", authRouter)
 module.exports = apiRouter
